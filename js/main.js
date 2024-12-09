dfe0e3bd1362016f5ae8485ace8967009b464b24
marcoaolivei <150826561+marcoaolivei@users.noreply.github.com>
2024-12-08 20:23:13 -0400

    Primeiro commit do exercício

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function(){
+	$('#carousel-imagens').slick({
+		autoplay: true,
+	});
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    });
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    });
 
-$('form').validate({
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    });
+
+    $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            required: true
         },
         email: {
-            requerido: true,
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            required: true
         },
         endereco: {
-            requerido: true
+            required: true
         },
         cep: {
-            requerido: true
+            required: true
         },
         cpf: {
-            requerido: true
-        },
+            required: true
+        }
     },
     submitHandler: function (form) {
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
@@ -39,4 +44,5 @@ $('form').validate({
     invalidHandler: function (form, validator) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
     }
-})
+});
+});
